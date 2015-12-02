9ad25d3fa1894140c3c9871c86e34b5ed76aeec7
wpc <alex.hal9000@gmail.com>
2015-12-01 16:26:58 -0800

    Initial Commit

---
+++ NeuralNet/NeuralNet.h
@@ -0,0 +1,19 @@
+//
+//  NeuralNet.h
+//  NeuralNet
+//
+//  Created by pwang on 12/1/15.
+//  Copyright © 2015 wpc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeuralNet.
+FOUNDATION_EXPORT double NeuralNetVersionNumber;
+
+//! Project version string for NeuralNet.
+FOUNDATION_EXPORT const unsigned char NeuralNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeuralNet/PublicHeader.h>
+
+
